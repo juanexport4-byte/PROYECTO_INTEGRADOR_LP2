0178c28f32fb2ed20a80d8182446fe3762e40dde
Samuel <samuelgualv@gmail.com>
2026-06-28 22:33:12 -0500

    CREAMOS CLASE

--- CODIGO/procesador.py
+++ CODIGO/procesador.py
@@ -1,3 +1,16 @@
 import pandas as pd
 import json
 import os
+
+class ProcesadorDatos:
+    """
+    Clase para procesar y limpiar los datos de ofertas de trabajo
+    """
+    
+    def __init__(self, archivo_json="datos/crudos/ofertas_hiringcafe.json"):
+        """
+        Inicializa el procesador cargando los datos del JSON
+        """
+        self.ruta_json = archivo_json
+        self.df = None
+        self._cargar_datos()
